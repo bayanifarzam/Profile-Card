affb1d2c5cbca5208df8818ba6eab2655e74d168
Raza Bayani <bayanifarzam@gmail.com>
2022-02-06 14:59:07 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}','*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'rubik':'Rubik',
+        'super':'super'
+      },
+      screens:{
+        'xs':'340px'
+      },
+      colors:{
+        'light-gold':"#f0f246cf",
+        'dark-gold':"#f0f246"
+      }
+      
+    },
+  },
+  plugins: [],
+}
